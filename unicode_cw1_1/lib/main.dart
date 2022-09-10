f7f8f41b730a2e1f950ae41d737cbb1664cb9f3a
zainab Ali <zainab.aliii.h@gmail.com>
2022-09-10 14:58:08 +0300

    .......

---
+++ unicode_cw1_1/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  var firstName = "zainab";
+  var lastName = "hussain";
+  var age = 20;
+  var hoppy = "iphonegraphy";
+
+  print("first name:  $firstName");
+  print("last name:  $lastName");
+  print("i'm $age my hoppy is $hoppy");
+}
